ae34a686a6014e835f6973ac734a4b713a436ccf
unknown <rjw88@naver.com>
2017-07-22 15:45:25 +0900

    4편 end

--- README.md
+++ README.md
@@ -6,5 +6,7 @@
 ## [1편 소개 및 맛보기](./notes/basics/1.md)
 ## [2편 작업환경 설정하기](./notes/basics/2.md)
 ## [3편 JSX](./notes/basics/3.md)
+## 4편 Component 생성 및 모듈화
 
-3편 End
+
+4편 End
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from './Header';
+import Content from './Content';
 
 class App extends React.Component {
     render(){
 
-        return (
-                <h1>Hello React Skeleton !!</h1>
+        return (                
+            <div>
+                <Header />
+                <Content />
+            </div>
         );
     }
 }
